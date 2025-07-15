ca91e263b957f3d7c7b9e0587167d58954c57569
RootERXc <nikita.kupriyanov.05@mail.ru>
2025-07-15 22:48:25 +0400

    Создание начальной структуры

--- cli.php
+++ cli.php
@@ -1,2 +1,124 @@
 <?
 
+trait Close 
+{
+    private function __construct ()
+    {
+    }
+
+    private function __clone ()
+    {
+    }
+
+    private function __wakeup ()
+    {
+    }
+
+    private function __sleep ()
+    {
+    }
+}
+
+final class Rounds 
+{
+    private static $rounds = array ();
+
+    public static function start () {
+        $types = getSubclasses ('Mode');
+        $select_type = NULL;
+
+        if (count($types) < 2)
+            if (count($types) == 1):
+                $select_type = $types[0];
+                goto skip_select_mode;
+            else:
+                echo 'Нету доступных режимов игры';
+                return;
+            endif;
+
+        echo 'Выберите режим:', PHP_EOL;
+
+        for ($i = 0; $i < count ($types); $i ++)
+        {
+            $type = $types [$i];
+
+            echo $i + 1, '. ', $type, PHP_EOL;
+        }
+
+        skip_select_mode:
+
+        echo $select_type;
+
+        // $difficulties = getSubclasses ('Difficulty');
+    }
+
+    use Close;
+}
+
+abstract class Mode
+{
+    // public static function start ()
+    // {
+    //     $round = new static ();
+
+    //     self::$rounds = [];
+    // }
+
+    public function selectType () 
+    {
+
+    }
+}
+
+class Standart extends Mode
+{
+}
+
+class Standart2 extends Mode
+{
+}
+
+abstract class Difficulty
+{
+    protected int $chances = 1;
+
+    public function selectDifficulty () 
+    {
+
+    }
+}
+
+class Easy extends Difficulty
+{
+    protected int $chances = 10;
+}
+
+class Medium extends Difficulty 
+{
+    protected int $chances = 5;
+}
+
+class Hard extends Difficulty 
+{
+    protected int $chances = 3;
+}
+
+function getSubclasses ($class) 
+{
+    $array = array ();
+
+    foreach (get_declared_classes () as $subclass) 
+    {
+        if (is_subclass_of ($subclass, $class))
+            $array[] = $subclass;
+    }
+
+    return $array;
+}
+
+Rounds::start();
+
+// var_dump(getSubclasses ('Difficulty'));
+
+// $test = new Round ();
+// Round::start ();
